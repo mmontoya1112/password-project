5a34852ff68feaf5b0b4cd61dffb619404dff437
michael montoya <michaelmontoya@michaels-MacBook-Pro.local>
2022-10-04 13:57:23 -0700

    added functionality for true logic

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var choice = [];
+var passwordLength = 8;
 var upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var special = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+'];
@@ -8,20 +9,49 @@ var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+   var truePrompts = prompts();
+   if(truePrompts) {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.value = password;
+   }
 
-  passwordText.value = password;
+  // var password = generatePassword();
+  // var passwordText = document.querySelector("#password");
+
+  // passwordText.value = password;
 
 }
 
 function generatePassword(){
-
+  var password = "";
+  for(var i = 0; i < passwordLength; i++) {
+    var randomLetter =
+  }
 }
 
 function prompts(){
-  characterLength = parseInt(prompt("How long do you want your password to be?"));
-}
+  choice = [];
+  passwordLength = parseInt(prompt("How long do you want your password to be?"));
+
+  if(isNaN(passwordLength) || passwordLength < 8 || passwordLength > 100) {
+    alert("password length must be an integer between 8-100. try again!");
+    return false;
+  };
+  if(confirm("do you want lowercase letters?")){
+    choice = choice.concat(lower);
+  };
+  if(confirm("do you want uppercase letters?")){
+    choice = choice.concat(upper);
+  };
+  if(confirm("do you want special letters?")){
+    choice = choice.concat(special);
+  }if(confirm("do you want numbers?")){
+    choice = choice.concat(numbers);
+  };
+  return true;
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
