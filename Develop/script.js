942d1a3e724baa6b4c8523a174f5c2b2cc32cf43
michael montoya <michaelmontoya@michaels-MacBook-Pro.local>
2022-10-04 13:43:55 -0700

    added prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var choice = [];
+var upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var special = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+'];
+var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 
 // Write password to the #password input
 function writePassword() {
@@ -8,6 +13,14 @@ function writePassword() {
 
   passwordText.value = password;
 
+}
+
+function generatePassword(){
+
+}
+
+function prompts(){
+  characterLength = parseInt(prompt("How long do you want your password to be?"));
 }
 
 // Add event listener to generate button
